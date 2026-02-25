ddfe04c0731f9bd0c6525c3d05c20e5d2aa7671b
lufreitas000 <freitas.physics@gmail.com>
2026-02-25 16:34:18 -0300

    feat: add AuditLog relational model for security tracking

--- src/auth/models.py
+++ src/auth/models.py
@@ -17,9 +17,29 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(50), unique=True, nullable=True)
     bio = db.Column(db.String(255), nullable=True)
     locked_until = db.Column(db.DateTime(timezone=True), nullable=True)
+    audit_logs = db.relationship('AuditLog', backref='user', lazy=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
+
+
+
+class AuditLog(db.Model):
+    __tablename__ = "audit_logs"
+
+    id = db.Column(db.Integer, primary_key=True)
+    # The Foreign Key links this log row directly to a specific user's ID
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    timestamp = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    ip_address = db.Column(db.String(45), nullable=True) # Supports IPv6
+    was_successful = db.Column(db.Boolean, nullable=False)
+
+    def __repr__(self):
+        return f'<AuditLog {self.user_id} - Success: {self.was_successful}>'
+
+
+
 
 # Flask-Login doesn't automatically know how to talk to SQLAlchemy. We have to give it a "Translator" function.
 # This tells Flask-Login how to find a user in the database using the ID from the cookie
